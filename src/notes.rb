29c6ea9976d235c29caca42ab8187312a1a93a04
Jeb Williams <jeb.williams8@gmail.com>
2019-11-21 16:59:00 +1000

    changes to all files

--- src/notes.rb
+++ src/notes.rb
@@ -1,7 +1,5 @@
 require_relative "notes_gems"
 
-
-
 class Page
 
     attr_accessor :datetime, :question, :note, :summary
@@ -14,31 +12,62 @@ class Page
 
 end 
 
-def enter_note(subject)
+def enter_note
 
     puts
-    new_page = Page.new
-    note = nil
+    puts("Select as subject or add a new subject:")
+    puts
+    choices = []
+    $subject.each_key{|subject| choices.push(subject)}
+    choices.push ("New Subject")
+    $user_input = $prompt.select("Please select from the menu:", choices)
 
-    loop do
-        
-        note = $prompt.ask('Enter note or [1] to exit to main menu:')
+    if $user_input == "New Subject"
+        subject = $prompt.ask('What is your subject?')
+        new_page = Page.new
         puts
-        if note == "1"
-            $subject = {subject => new_page}
-            break
-        else
-            new_page.note.push(note)
-            new_page.question.push($prompt.ask('Enter a question related to the note:'))
+        note = nil
+        
+        loop do
+            note = $prompt.ask('Enter note or [1] to exit to main menu:')
             puts
+            if note == "1"
+                $subject[subject] = new_page
+                break
+            else
+                new_page.datetime.push(Time.now)
+                new_page.note.push(note)
+                new_page.question.push($prompt.ask('Enter a question related to the note:'))
+                puts
+            end
         end
 
-    end
+    else 
+
+        puts
+        note = nil
+        
+        loop do
+            note = $prompt.ask('Enter note or [1] to exit to main menu:')
+            puts
+            subject_obj = $subject[$user_input]
+            
+            if note == "1"
+                $subject[$user_input] = subject_obj
+                break
+            else
+                subject_obj.datetime.push(Time.now)
+                subject_obj.note.push(note)
+                subject_obj.question.push($prompt.ask('Enter a question related to the note:'))
+                puts
+            end
+        end
     
+    end
 end
 
 def delete_note 
-
+    
 end
 
 def input_notes
@@ -64,7 +93,7 @@ def review_note
 
     puts
     table = Terminal::Table.new :title => $user_input,
-                                :headings => ['Date', 'Question', 'Note'],
+                                :headings => ['DATE', 'QUESTION', 'NOTE'],
                                 :alignment => :left 
                            
     for index in 0...datetime.length
@@ -72,10 +101,13 @@ def review_note
         table.add_row [datetime_out, question[index], note[index]]
         table.style = {:all_separators => true, :padding_left => 3,:alignment => :center}
     end
- 
+    
+    puts
     puts table
     puts
-    
+
+    output_review_note (table)
+
 end
 
 def quiz
@@ -111,53 +143,52 @@ def quiz
 
 end
 
+def output_review_note (table)
+
+    $user_input = $prompt.yes?('do you wish to export?')
+        
+    if $user_input == true
+        aFile = File.new("notes.txt", "r+")
+        if aFile
+            aFile.syswrite(table)
+        else
+            puts "Unable to open file!"
+        end
+    end
+        
+    puts
+
+end
 
 $subject  = Hash.new
 $font = TTY::Font.new(:standard)
 $prompt = TTY::Prompt.new
+
+#puts Time.now
 
 input_notes
 
 puts $font.write("Notes").green
 puts
 
-
-
 $user_input = nil
   
 loop do
     
- 
     begin
 
-        choices = ["Enter Notes", "Review Notes", "Quiz", "Exit"]
+        choices = ["Enter Notes", "Review Notes", "Delete Note", "Quiz", "Exit"]
         $user_input = $prompt.select("Please select from the menu:", choices)
         
         case $user_input
         when "Enter Notes"
-            
-            puts
-            puts("Select as subject or add a new subject:")
-            puts
-            choices = []
-            $subject.each_key{|subject| choices.push(subject)}
-            choices.push ("New Subject")
-            $user_input = $prompt.select("Please select from the menu:", choices)
-        
-            if $user_input == "New Subject"
-            
-                subject = $prompt.ask('What is your subject?')
-                $subject = {subject => nil}
-                enter_note (subject)
-            
-            else 
-                enter_note ($user_input)
-            end
-
-        when "Quiz"
-           quiz
+            enter_note
         when "Review Notes"
             review_note
+        when "Delete Note"
+            delete_note
+        when "Quiz"
+           quiz
         when "Exit"
             break
         else 
@@ -173,6 +204,7 @@ loop do
         puts
 
     ensure
+        puts
         output_note
     end 
     
